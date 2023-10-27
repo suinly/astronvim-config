7e8149ff328390fb7b20b93e84ee93a737d345b7
Daniel Flow <thedanielflow@gmail.com>
2023-10-27 17:42:57 +0300

    Update neorg config

--- plugins/neorg.lua
+++ plugins/neorg.lua
@@ -7,7 +7,13 @@ return {
     load = {
       ["core.defaults"] = {}, -- Loads default behaviour
       ["core.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.keybinds"] = {
+        -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+        config = {
+          default_keybinds = true,
+          neorg_leader = "<Leader><Leader>",
+        },
+      }, 
       ["core.completion"] = {
         config = {
           engine = "nvim-cmp",
@@ -17,8 +23,10 @@ return {
       ["core.dirman"] = { -- Manages Neorg workspaces
         config = {
           workspaces = {
-            fastanime = "~/Projects/fast-anime/notes",
-            turclubpik = "~/Projects/turclub-pik/notes"
+            private = "~/Neorg/private",
+            fastanime = "~/Neorg/fast-anime",
+            xlmedia = "~/Neorg/xlmedia",
+            turclubpik = "~/Neorg/turclubpik",
           },
         },
       },
